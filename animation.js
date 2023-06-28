bd68450a769a2828a84dd0b00b33875129d04833
Stephen <stephenbusscher@gmail.com>
2023-06-28 19:43:45 -0400

    nvm php. it sucks

--- animation.js
+++ animation.js
@@ -4,10 +4,11 @@ window.addEventListener("beforeunload", function () {
   window.scrollTo(0, 0);
 });
 
-// Code for greeting animation
+// greeting animation
 
 window.addEventListener("load", () => {
   const greeting = document.getElementById("animation");
+  const quote = document.querySelector(".quote");
   let opacity = 0;
   const duration = 2600;
   let start = null;
@@ -23,12 +24,17 @@ window.addEventListener("load", () => {
   };
   window.requestAnimationFrame(fadeIn);
 
+  const quoteFadeFactor = 2;
   const fadeFactor = 2.5;
   window.addEventListener("scroll", () => {
     const scrolled = window.pageYOffset;
     const windowHeight = window.innerHeight;
-    const newOpacity = Math.max(1 - (scrolled / windowHeight) * fadeFactor, 0);
-    greeting.style.opacity = newOpacity;
+    const quoteOpacity = Math.max(
+      (scrolled / windowHeight) * quoteFadeFactor,
+      0
+    );
+    const otherOpacity = Math.max((scrolled / windowHeight) * fadeFactor, 0);
+    quote.style.opacity = quoteOpacity > 1 ? 1 : quoteOpacity;
   });
 });
 
@@ -36,3 +42,22 @@ function cubicEaseInOut(t) {
   if ((t /= 0.5) < 1) return 0.5 * Math.pow(t, 3);
   return 0.5 * (Math.pow(t - 2, 3) + 2);
 }
+
+// darken video
+
+window.addEventListener("load", () => {
+  const videoBg = document.getElementById("video-bg");
+  const windowHeight = window.innerHeight;
+  const fadeFactor = 1.5;
+
+  function adjustVideoOpacity() {
+    const scrolled = window.pageYOffset;
+    const scrollRatio = scrolled / windowHeight;
+    const newOpacity = Math.max(1 - scrollRatio * fadeFactor, 0);
+    videoBg.style.opacity = newOpacity.toString();
+  }
+
+  adjustVideoOpacity();
+
+  window.addEventListener("scroll", adjustVideoOpacity);
+});
