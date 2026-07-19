5d63ed53c54b2fbccc857cb651fbe7a41dde8ff4
Stephen Busscher <stephenbusscher@gmail.com>
2026-07-19 11:26:51 -0400

    Revert "Replace Font Awesome with local icons"
    This reverts commit 69259384c68dc7e9d22bb04fcb4383a6a8dce3b8.

--- catalog.js
+++ catalog.js
@@ -253,8 +253,7 @@ const createUserCards = (data) => {
     });
 
     const wishlistButton = document.createElement("button");
-    wishlistButton.innerHTML =
-      '<span class="icon icon-heart" aria-hidden="true"></span>';
+    wishlistButton.innerHTML = '<i class="fas fa-heart" aria-hidden="true"></i>';
     wishlistButton.setAttribute("aria-label", `Add ${name} to wishlist`);
     wishlistButton.setAttribute(
       "aria-pressed",
